ae5fa9cfff1fa69532b478a52ab8f415ad886b37
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-05 21:27:52 +0800

    refactor

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -9,6 +9,7 @@ describe.only('the fahrenheit2celsius canary spec', () => {
     it('give 0°C for 32°F', () => {
       fahrenheit2celsius(32).should.equal(0);
     });
+
     it('give 10°C for 50°F');
   });
 });
