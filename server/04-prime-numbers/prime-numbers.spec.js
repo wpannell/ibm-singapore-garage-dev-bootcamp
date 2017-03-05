2c5f52e9f58f9095ba2a318e8d63652524968ccd
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-06 05:55:52 +0800

    design prime number generator

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -3,6 +3,15 @@ describe('the prime numbers canary spec', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  describe('prime numbers of', () => {
+    it('"1" are none');
+    it('"2" is only "2"');
+    it('"3" is only "3"');
+    it('"4" are  "2" and "2"');
+    it('"5" is only "5"');
+    it('"6" are  "2" and "3"');
+    it('"7" is only "7"');
+    it('"8" are  "2," "2" and "2"');
+    it('"9" are  "3" and "3"');
   });
 });
