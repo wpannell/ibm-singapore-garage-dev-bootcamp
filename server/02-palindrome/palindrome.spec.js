ee8c58c1592c12b2b7154cf090226e1567434e33
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-05 21:42:21 +0800

    show the test list and canary spec for palindrome suite

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,10 +1,15 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
   describe('palindrome should be', () => {
-    it('true for mom');
-    it('false for dude');
+    it('true for "mom"');
+    it('false for "dude"');
+    it('true for "mom dad mom"');
+    it('false for "dude dude dude dude"');
+    it('"must be a word or phrase" error— for whitespace');
+    it('"cannot be null" error— for null');
+    it('"no argument" error— when no argument is passed');
   });
 });
