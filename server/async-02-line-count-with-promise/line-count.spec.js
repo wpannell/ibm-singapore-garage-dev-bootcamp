8ed806405a6f45a4ee2227a0a2dc98b79f6fe626
Matt Oberlin <moberlin8@gmail.com>
2017-03-06 15:50:43 +0800

    Async, with promises. All green.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/async-02-line-count-with-promise/line-count.spec.js
+++ server/async-02-line-count-with-promise/line-count.spec.js
@@ -1,8 +1,30 @@
+import {lineCount} from './line-count';
+
 describe('line count can be checked', () => {
+  const filespec = 'server/async-02-line-count-with-promise/line-count.js';
 
-  it('with the done parameter like with a callback');
+  it('with the done parameter like with a callback', done => {
 
-  it('by returning the promise after .then');
+    const receivedCount = (numberOfLines) => {
+      numberOfLines.should.equal(14);
+      done();
+    };
 
-  it('by using eventually or fulfilledWith');
+    lineCount(filespec).then(receivedCount);
+  });
+
+  it('by returning the promise after .then', () => {
+
+    const receivedCount = (numberOfLines) => {
+      numberOfLines.should.equal(14);
+    };
+
+    return lineCount(filespec).then(receivedCount);
+
+  });
+
+  it('by using eventually or fulfilledWith', () => {
+    return lineCount(filespec).should.eventually.equal(14);
+  });
+
 });
