42bcfdb5bf764d1b9dd0277339e812523b484fa7
dayhunter <patis.pn@gmail.com>
2017-03-06 12:10:44 +0800

    movie amountFor

--- server/05-martin-fowler-movie-example/makeCustomerFrom.js
+++ server/05-martin-fowler-movie-example/makeCustomerFrom.js
@@ -1,4 +1,6 @@
-import {REGULAR, NEW_RELEASE, CHILDRENS} from './movie-codes';
+import {NEW_RELEASE} from './movie-codes';
+import {amountFor} from './amount-for';
+
 
 let makeCustomerFrom = (name) => {
   let rentals = [];
@@ -13,35 +15,9 @@ let makeCustomerFrom = (name) => {
       let statement = 'Rental Record for ' + name + '\n';
 
       for (let i = 0; i < rentals.length; i++) {
-        let subTotal = 0;
         let rental = rentals[i];
-
+        let subTotal = amountFor(rental);
         //noinspection Eslint
-        switch (rental.movie.priceCode) {
-          case REGULAR:
-
-            subTotal += 2;
-            if (rental.daysRented > 2) {
-              subTotal += (rental.daysRented - 2) * 1.5;
-            }
-            break;
-
-          case NEW_RELEASE:
-
-            subTotal += rental.daysRented * 3;
-            break;
-
-          case CHILDRENS:
-
-            subTotal += 1.5;
-            if (rental.daysRented > 3) {
-              subTotal += (rental.daysRented - 3) * 1.5;
-            }
-
-            break;
-          default:
-
-        }
 
         // add frequent renter points
         frequentRenterPoints++;
