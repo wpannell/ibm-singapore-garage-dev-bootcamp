646f0cb320fe735cb86a1139410a86d4ea169c85
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-06 10:15:51 +0800

    porting over code

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -3,6 +3,20 @@ describe('the prime numbers canary spec', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  const primeFactorsOf = () => [];
+
+  describe('prime factors of', () => {
+    it('1 is none', () => {
+      primeFactorsOf(1).should.deepEqual([]);
+    });
+    it('2 are only 2');
+    it('3 are only 3');
+    it('4 are 2 and 2');
+    it('5 are only 5');
+    it('6 are 2 and 3');
+    it('7 are only 7');
+    it('8 are 2, 2 and 2');
+    it('9 are 3 and 3');
+    it('10 are 2 and 5');
   });
 });
