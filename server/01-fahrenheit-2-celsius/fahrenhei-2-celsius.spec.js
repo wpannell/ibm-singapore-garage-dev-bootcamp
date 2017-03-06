ee566db085d58efb0b29a886db976d9cf57ee871
Aldred Benedict <baldred@id.ibm.com>
2017-03-06 09:29:30 +0800

    Added Fahrenheit 2 Celcius

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,18 @@
 describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  const fahrenheit2celcius = (temperature) => {
+    return (temperature - 32) * 5 / 9;
+  };
 
   describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
-    it('give 10°C for 50°F');
+    it('gives 0°C for 32°F', () => {
+      fahrenheit2celcius(32).should.equal(0);
+    });
+    it('gives 10°C for 50°F', () => {
+      fahrenheit2celcius(50).should.equal(10);
+    });
   });
 });
