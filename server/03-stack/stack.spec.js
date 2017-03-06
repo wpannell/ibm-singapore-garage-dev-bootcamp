101b95e1c3bc04181aba1dc8899d67a5b695a552
Matt Oberlin <moberlin8@gmail.com>
2017-03-06 09:38:42 +0800

    Stack is gree, with tests pending.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -3,6 +3,36 @@ describe('the stack spec', () => {
     true.should.be.true();
   });
 
+  const stack = {
+    isEmpty: () => true,
+    push: () => {},
+    pop: () => {},
+    size: () => 1
+  };
+
   describe('a stack should', () => {
+    it('be empty on create', () => {
+      stack.isEmpty().should.be.true();
+
+    });
+
+    it('size one on push', () => {
+      stack.push(1);
+      stack.size().should.equal(1);
+    });
+
+    it('empty on push and pop', () => {
+      stack.push(1);
+      stack.pop(1);
+      stack.isEmpty().should.be.true();
+
+    });
+
+    it('overflow');
+    it('underflow');
+    it('pop the same element pushed');
+    it('pop the same two elements pushed');
+    it('error on illegal capacity');
   });
 });
+
