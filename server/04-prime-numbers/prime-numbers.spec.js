c65a7dd626a0f9b62e34a2fcfd7cb60437c15500
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-06 11:02:37 +0800

    make the 4th test pass

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,8 +1,41 @@
-describe('the prime numbers canary spec', () => {
+/*eslint no-param-reassign: "off"*/
+describe.only('the prime numbers canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  const primeFactorsOf = (number) => {
+    let factors = [];
+    if (number > 1) {
+      if (number % 2 === 0) {
+        factors.push(2);
+        number /= 2;
+      }
+      if (number > 1) factors.push(number);
+    }
+    return factors;
+  };
+
+  describe('prime factors of', () => {
+    it('1 is none', () => {
+      primeFactorsOf(1).should.deepEqual([]);
+    });
+
+    it('2 are only 2', () => {
+      primeFactorsOf(2).should.deepEqual([2]);
+    });
+
+    it('3 are only 3', () => {
+      primeFactorsOf(3).should.deepEqual([3]);
+    });
+
+    it('4 are 2 and 2', () => {
+      primeFactorsOf(4).should.deepEqual([2, 2]);
+    });
+    it('5 are only 5');
+    it('6 are 2 and 3');
+    it('7 are only 7');
+    it('8 are 2, 2 and 2');
+    it('9 are 3 and 3');
   });
 });
