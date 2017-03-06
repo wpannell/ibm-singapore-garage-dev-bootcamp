1521ff8b98ea99efffb7ef4261965351ee052d7d
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-06 12:06:09 +0800

    moved amountFor() to its own js file

--- server/05-martin-fowler-movie-example/makeCustomerFrom.js
+++ server/05-martin-fowler-movie-example/makeCustomerFrom.js
@@ -1,31 +1,5 @@
 import {REGULAR, NEW_RELEASE, CHILDRENS} from './movie-codes';
-
-let amountFor = (each) => {
-  let subTotal = 0;
-  //noinspection Eslint
-  switch (each.movie.priceCode) {
-    case REGULAR:
-      subTotal += 2;
-      if (each.daysRented > 2) {
-        subTotal += (each.daysRented - 2) * 1.5;
-      }
-      break;
-
-    case NEW_RELEASE:
-      subTotal += each.daysRented * 3;
-      break;
-
-    case CHILDRENS:
-      subTotal += 1.5;
-      if (each.daysRented > 3) {
-        subTotal += (each.daysRented - 3) * 1.5;
-      }
-      break;
-
-    default:
-  }
-  return subTotal;
-};
+import {amountFor} from './amount-for';
 
 let makeCustomerFrom = (name) => {
   let rentals = [];
