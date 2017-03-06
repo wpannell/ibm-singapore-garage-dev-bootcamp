af5bffd29cc20592ab4ac56e009a1f2d15f7cc7b
Jason Calalang <jasoncalalang@Jasons-MacBook-Pro.local>
2017-03-06 11:05:58 +0800

    working for conditions 1, 2, 3, 4, and 5

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -29,7 +29,9 @@ describe.only('the prime numbers canary spec', () => {
     it('4 are 2 and 2', () => {
       primeFactorsOf(4).should.deepEqual([2, 2]);
     });
-    it('5 are only 5');
+    it('5 are only 5', () => {
+      primeFactorsOf(5).should.deepEqual([5]);
+    });
     it('6 are 2 and 3');
     it('7 are only 7');
     it('8 are 2, 2, and 2');
