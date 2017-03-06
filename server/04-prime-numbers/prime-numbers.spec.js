02ee198c8e007a96d560cc30487cb6aa23fe4335
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-06 11:09:27 +0800

    make 7th test pass

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -41,7 +41,10 @@ describe.only('the prime numbers canary spec', () => {
       primeFactorsOf(6).should.deepEqual([2, 3]);
     });
 
-    it('7 are only 7');
+    it('7 are only 7', () => {
+      primeFactorsOf(7).should.deepEqual([7]);
+    });
+
     it('8 are 2, 2 and 2');
     it('9 are 3 and 3');
   });
