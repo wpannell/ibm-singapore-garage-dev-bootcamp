1f92ff69aaf95e75db453535a1740ecb5e25bdbb
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-06 11:35:33 +0800

    made the rest of the tests past

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -6,11 +6,11 @@ describe('the prime numbers canary spec', () => {
 
   const primeFactorsOf = (number) => {
     const factors = [];
-    let divisor = 2;
-    for (; number > 1; divisor++) {
+
+    for (let divisor = 2; number > 1; divisor++) {
       for (; number % divisor === 0; number /= divisor) factors.push(divisor);
     }
-    if (number > 1) factors.push(number);
+
     return factors;
   };
 
