20951fd70edff0abf31d1c0b9ad4ac2909f08cc7
WIL PANNELL <wgpannel@us.ibm.com>
2017-03-06 09:42:12 +0800

    design for unusual-spending

---
+++ server/06-unusual-spending/unusual-spending-spec.js
@@ -0,0 +1,10 @@
+/*eslint dot-notation: "off"*/
+import {replace, when, verify} from '../../test-helper';
+
+describe('unusual spending should', () => {
+  it('canary test shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  it('interact with fetch, high-spending and email');
+});
