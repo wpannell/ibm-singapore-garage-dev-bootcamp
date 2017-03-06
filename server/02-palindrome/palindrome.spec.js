a93cf40a6e25e04cb81a347cdca020da60b0e9f2
Aldred Benedict <baldred@id.ibm.com>
2017-03-06 09:30:55 +0800

    Added Palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,46 @@ describe('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
+  let isPalindrome = (phrase) => {
+    if (typeof phrase === 'undefined') throw new Error('phrase is missing');
+    if (phrase === null) throw new Error('null is not a valid input');
+    if (typeof phrase !== 'string') throw new Error('phrase must be string');
+
+    const reversed = phrase.split('').reverse().join('');
+    return reversed === phrase;
+  };
+
   describe('palindrome should be', () => {
-    it('true for mom');
-    it('false for dude');
+    it('true for mom', () => {
+      isPalindrome('mom').should.be.true();
+    });
+
+    it('false for "dude', () => {
+      isPalindrome('dude').should.be.false();
+    });
+
+    it('true for "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
+
+    it('false for "any phrase"', () => {
+      isPalindrome('any phrase').should.be.false();
+    });
+
+    it('an error when you pass nothing', () => {
+      (() => {
+        isPalindrome();
+      }).should.throw('phrase is missing');
+    });
+    it('an error when you pass null', () => {
+      (() => {
+        isPalindrome(null);
+      }).should.throw('null is not a valid input');
+    });
+    it('an error when a string is not passed', () => {
+      (() => {
+        isPalindrome(65);
+      }).should.throw('phrase must be string');
+    });
   });
 });
