e9b2b8d9e161574aea4e3b90e8700f07207c8bb1
TAE MIN KO <tmk@kr.ibm.com>
2017-03-06 14:38:29 +0800

    draft refactoring done

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -1,6 +1,19 @@
-describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+import {lineCount} from './line-count.js';
 
+describe('line count', () => {
+  const filespec = 'server/async-01-line-count-with-callback/line-count.js';
+
+  it('for file —— line-count.js —— should be 13', done => {
+
+    function onSuccessReceiveLineCount(numberOfLines) {
+      numberOfLines.should.equal(13);
+      done();
+    }
+
+    //const lineCount = (filename, onError, onSuccess) => {
+    lineCount(filespec, null, onSuccessReceiveLineCount);
+  });
   it('for file —— is-not-there.js —— should be problem reading file: ' +
       'is-not-there.js');
 });
+
