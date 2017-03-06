15eb7e20e82def4fa885320dfcf6ebc14936bfae
Matt Oberlin <moberlin8@gmail.com>
2017-03-06 09:31:28 +0800

    Fahrenheit2Celcius works.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,20 @@
 describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  const fahrenheight2celcius = (temperature) => {
+    return (temperature - 32) * 5 / 9;
+  };
 
   describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
-    it('give 10°C for 50°F');
+    it('give 0°C for 32°F', () => {
+      fahrenheight2celcius(32).should.equal(0);
+    });
+
+    it('give 10°C for 50°F', () => {
+      fahrenheight2celcius(50).should.equal(10);
+    });
+
   });
 });
