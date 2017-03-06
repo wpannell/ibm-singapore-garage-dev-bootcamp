3540608d59f854bca8ca584c6b9ff964474a835f
JoongYeop Kim <jyeop@kr.ibm.com>
2017-03-06 14:54:29 +0800

    porting over code

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -2,8 +2,8 @@ import {lineCount} from './line-count';
 
 describe.only('line count', () => {
   const filespec = 'server/async-01-line-count-with-callback/line-count.js';
-  it('for file —— line-count.js —— should be 13', done => {
 
+  it('for file —— line-count.js —— should be 13', done => {
     function onSuccessReceiveLineCount(numberOfLines) {
       numberOfLines.should.equal(13);
       done();
@@ -13,5 +13,12 @@ describe.only('line count', () => {
   });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
-      'is-not-there.js');
+      'is-not-there.js', done => {
+    const OnErrorReceiveMessage = (message) => {
+      message.should.equal('problem reading file: is-not-there.js');
+      done();
+    };
+
+    lineCount('is-not-there.js', OnErrorReceiveMessage, null);
+  });
 });
