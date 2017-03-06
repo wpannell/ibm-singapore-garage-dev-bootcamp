3650616d0907733bbcee46e1779dec97e6be1b51
JoongYeop Kim <jyeop@kr.ibm.com>
2017-03-06 11:02:24 +0800

    porting over code

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,8 +1,45 @@
-describe('the prime numbers canary spec', () => {
+/* eslint no-param-reassign: "off" */
+describe.only('the prime numbers canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  const primeFactorsOf = (number) => {
+    const factors = [];
+
+    if (number > 1) {
+      if (number % 2 === 0) {
+        factors.push(2);
+        number /= 2;
+      }
+      if (number > 1) factors.push(number);
+    }
+    /*
+    for (let divisor = 2; number > 1; divisor++) {
+      for (; number % divisor === 0; number /= divisor) factors.push(divisor);
+    }
+    */
+    return factors;
+  };
+
+  describe('a prime numbers of', () => {
+    it('1 is empty', () => {
+      primeFactorsOf(1).should.deepEqual([]);
+    });
+    it('2 is 2', () => {
+      primeFactorsOf(2).should.deepEqual([2]);
+    });
+
+    it('3 is 3', () => {
+      primeFactorsOf(3).should.deepEqual([3]);
+    });
+    it('4 is 2 and 2', () => {
+      primeFactorsOf(4).should.deepEqual([2, 2]);
+    });
+    it('5 are only 5');
+    it('6 are 2 and 3');
+    it('7 are only 7');
+    it('8 are 2, 2 and 2');
+    it('9 are 3');
   });
 });
