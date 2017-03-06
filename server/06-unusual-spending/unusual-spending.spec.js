b57ff6ea21a0505ed490a641a4229bea07e50f04
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-06 15:50:11 +0800

    added sync tests to line-count.spec.js

--- server/06-unusual-spending/unusual-spending.spec.js
+++ server/06-unusual-spending/unusual-spending.spec.js
@@ -10,21 +10,20 @@ describe('unusual spending spec', () => {
         'categorize and email', (done) => {
       const fetch = td.replace('./fetch').fetch;
       const categorize = td.replace('./categorize').categorize;
-      //const email = td.replace('./email').email;
+      const email = td.replace('./email').email;
 
       td.when(fetch('dummy-id')).thenResolve('dummy-payments');
       td.when(categorize('dummy-payments')).thenReturn('dummy-categorized-payments');
 
       const unusualSpending = require('./unusual-spending').unusualSpending;
       unusualSpending('dummy-id');
-      done();
 
-      // setInterval(() => {
-      //   td.verify(fetch('dummy-id'));
-      //   td.verify(categorize('dummy-payments'));
-      //   td.verify(email('dummy-id', 'dummy-categorized-payments'));
-      //   done();
-      // }, 20);
+      setInterval(() => {
+        td.verify(fetch('dummy-id'));
+        td.verify(categorize('dummy-payments'));
+        td.verify(email('dummy-id', 'dummy-categorized-payments'));
+        done();
+      }, 20);
     });
   });
 });
