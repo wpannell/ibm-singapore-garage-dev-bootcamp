608cdb80c00cdd79323274811604c43ba2337722
Aldred Benedict <baldred@id.ibm.com>
2017-03-06 14:36:21 +0800

    Created line-count

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -1,5 +1,15 @@
-describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+import {lineCount} from './line-count';
+
+describe.only('line count', () => {
+  const filespec = 'server/async-01-line-count-with-callback/line-count.js';
+  it('for file —— line-count.js —— should be 13', (done) => {
+    function onSuccessReceiveLineCount(noOfLines) {
+      noOfLines.should.equal(13);
+      done();
+    }
+
+    lineCount(filespec, null, onSuccessReceiveLineCount);
+  });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
       'is-not-there.js');
