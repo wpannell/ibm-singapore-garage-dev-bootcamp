a2df046bf362905f024a299502e7ceafe548ceb6
Matt Oberlin <moberlin8@gmail.com>
2017-03-06 09:33:56 +0800

    Palindrome works.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,49 @@ describe('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
+  let isPalindrome = (phrase) => {
+    if (typeof phrase === 'undefined') throw new Error('phrase is missing');
+    if (phrase === null) throw new Error('phrase cannot be null');
+    if (typeof phrase !== 'string') throw new Error('phrase must be a string');
+
+    const reversed = phrase.split('').reverse().join('');
+
+    return phrase === reversed;
+  };
+
   describe('palindrome should be', () => {
-    it('true for mom');
-    it('false for dude');
+    it('true for "mom"', () => {
+      isPalindrome('mom').should.be.true();
+    });
+
+    it('false for "dude"', () => {
+      isPalindrome('dude').should.be.false();
+    });
+
+    it('true for "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
+
+    it('false for "any phrase"', () => {
+      isPalindrome('any phrase').should.be.false();
+    });
+
+    it('error when you pass nothing', () => {
+      (() => {
+        isPalindrome();
+      }).should.throw('phrase is missing');
+    });
+
+    it('error when you pass null', () => {
+      (() => {
+        isPalindrome(null);
+      }).should.throw('phrase cannot be null');
+    });
+
+    it('error when a string is not passed', () => {
+      (() => {
+        isPalindrome({});
+      }).should.throw('phrase must be a string');
+    });
   });
 });
