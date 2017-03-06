3b829e661ec7a82249e8d320330f9b701a758c1e
JoongYeop Kim <jyeop@kr.ibm.com>
2017-03-06 09:40:12 +0800

    porting over code

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,18 @@
 describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  const fahrenheit2celsius = (temperature) => {
+    return (temperature - 32) * 5 / 9;
+  };
 
   describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
-    it('give 10°C for 50°F');
+    it('give 0°C for 32°F', () => {
+      fahrenheit2celsius(32).should.be.equal(0);
+    });
+    it('give 10°C for 50°F', () => {
+      fahrenheit2celsius(50).should.equal(10);
+    });
   });
 });
