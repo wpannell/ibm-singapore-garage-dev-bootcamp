0195adc43e1dfb8419765ab5d72787efa0136576
JoongYeop Kim <jyeop@kr.ibm.com>
2017-03-06 09:58:03 +0800

    porting over code

--- server/06-unusual-spending/fetch.js
+++ server/06-unusual-spending/fetch.js
@@ -2,12 +2,9 @@ import {current, prior} from './months';
 import {api} from './api';
 
 const fetch = (userId) => {
-
   const currentMonth = current();
   const priorMonth = prior();
-
   const fetchCurrentMonth = api(userId, currentMonth);
   const fetchPriorMonth = api(userId, priorMonth);
-
-  export {fetch};
 };
+export {fetch};
