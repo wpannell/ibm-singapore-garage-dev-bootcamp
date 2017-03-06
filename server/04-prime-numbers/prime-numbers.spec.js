e5031581fb624f6d1a50ffb807d86ce052f87c0f
Aldred Benedict <baldred@id.ibm.com>
2017-03-06 10:46:28 +0800

    Added logic to pass primeFactorsOf(2)

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,8 +1,27 @@
-describe('the prime numbers canary spec', () => {
+describe.only('the prime numbers canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  const primeFactorsOf = (number) => {
+    let factors = [];
+    if (number > 1) factors.push(2);
+    return factors;
+  };
+
+  describe('prime factors of', () => {
+    it('1 is none', () => {
+      primeFactorsOf(1).should.deepEqual([]);
+    });
+    it('2 are only 2', () => {
+      primeFactorsOf(2).should.deepEqual([2]);
+    });
+    it('3 are only 3');
+    it('4 are 2 and 2');
+    it('5 are only 5');
+    it('6 are 2 and 3');
+    it('7 are only 7');
+    it('8 are 2, 2 and 2');
+    it('9 are 3 and 3');
   });
 });
