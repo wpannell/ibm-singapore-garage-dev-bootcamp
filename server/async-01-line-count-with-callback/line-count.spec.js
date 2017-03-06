48e470d855199ba69e70602e69ba8573a188e825
Matt Oberlin <moberlin8@gmail.com>
2017-03-06 14:36:17 +0800

    First test passes async
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -1,5 +1,17 @@
+import {lineCount} from './line-count';
+
 describe('line count', () => {
-  it('for file —— line-count.js —— should be 13');
+  const filespec = 'server/async-01-line-count-with-callback/line-count.js';
+
+  it('for file —— line-count.js —— should be 13', done => {
+
+    function onSuccessReceiveLineCount(numberOfLines) {
+      numberOfLines.should.equal(13);
+      done();
+    }
+
+    lineCount(filespec, null, onSuccessReceiveLineCount);
+  });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
       'is-not-there.js');
