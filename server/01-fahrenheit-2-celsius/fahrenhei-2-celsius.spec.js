5d9da1081530c17dd394033257afc707c550cbc6
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-06 09:29:48 +0800

    porting over code

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,29 @@
+const fahrenheit2celsius = (temperature) => {
+  return (temperature - 32) * 5 / 9;
+};
+
 describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+  const PRECISION = 0.01;
+
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
 
   describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
-    it('give 10°C for 50°F');
+    it('give 0°C for 32°F', () => {
+      fahrenheit2celsius(32).should.equal(0);
+    });
+
+    it('give 10°C for 50°F', () => {
+      fahrenheit2celsius(50).should.equal(10);
+    });
+
+    it('give -40°C for -40°F', () => {
+      fahrenheit2celsius(-40).should.equal(-40);
+    });
+
+    it('give -17.78°C for 0°F', () => {
+      fahrenheit2celsius(0).should.be.approximately(-17.78, PRECISION);
+    });
   });
 });
