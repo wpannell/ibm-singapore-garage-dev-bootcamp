749a6027673ce6c2bcb3e17124c9321a648b91af
Jason Calalang <jasoncalalang@Jasons-MacBook-Pro.local>
2017-03-06 10:52:03 +0800

    working for conditions 1 2 and 3
    Signed-off-by: Jason Calalang <jasoncalalang@Jasons-MacBook-Pro.local>

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,11 +1,12 @@
-describe('the prime numbers canary spec', () => {
+describe.only('the prime numbers canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
   const primeFactorsOf = (number) => {
     let factors = [];
-    if (number > 1) factors.push(2);
+
+    if (number > 1) factors.push(number);
     return factors;
   };
 
@@ -16,7 +17,9 @@ describe('the prime numbers canary spec', () => {
     it('2 are only 2', () => {
       primeFactorsOf(2).should.deepEqual([2]);
     });
-    it('3 are only 3');
+    it('3 are only 3', () => {
+      primeFactorsOf(3).should.deepEqual([3]);
+    });
     it('4 are 2 and 2');
     it('5 are only 5');
     it('6 are 2 and 3');
