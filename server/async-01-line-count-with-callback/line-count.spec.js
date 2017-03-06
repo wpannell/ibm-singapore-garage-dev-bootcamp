7ed17c084297798057ea814c1e18f4018eb16d9d
dayhunter <patis.pn@gmail.com>
2017-03-06 14:50:55 +0800

    async success and error

--- server/async-01-line-count-with-callback/line-count.spec.js
+++ server/async-01-line-count-with-callback/line-count.spec.js
@@ -1,13 +1,20 @@
 import {lineCount} from './line-count';
 describe.only('line count', () => {
+  const filespec = 'server/async-01-line-count-with-callback/line-count.js';
   it('for file —— line-count.js —— should be 13', done => {
     function onSuccessReceiveLineCount(numberOfLines) {
       numberOfLines.should.be.equal(13);
       done();
-    };
-    lineCount('server/async-01-line-count-with-callback/line-count.js', null, onSuccessReceiveLineCount);
+    }
+    lineCount(filespec, null, onSuccessReceiveLineCount);
   });
 
   it('for file —— is-not-there.js —— should be problem reading file: ' +
-      'is-not-there.js');
+      'is-not-there.js', done => {
+    const onErrorReceiveLineCount = (message) => {
+      message.should.be.equal('problem reading file: is-not-there.js');
+      done();
+    };
+    lineCount('is-not-there.js', onErrorReceiveLineCount, null);
+  });
 });
