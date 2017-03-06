fba6638ee3ec197f1d744bc0fb8f990ddbe68092
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-06 12:25:05 +0800

    moved amountFor() to rental class

--- server/05-martin-fowler-movie-example/makeRentalFrom.js
+++ server/05-martin-fowler-movie-example/makeRentalFrom.js
@@ -1,8 +1,35 @@
-let makeRentalFrom = (movie, daysRented) => {
+import {REGULAR, NEW_RELEASE, CHILDRENS} from './movie-codes';
 
+let makeRentalFrom = (movie, daysRented) => {
   return {
     get movie() { return movie; },
-    get daysRented() { return daysRented; }
+    get daysRented() { return daysRented; },
+    amountFor: () => {
+      let subTotal = 0;
+      //noinspection Eslint
+      switch (movie.priceCode) {
+        case REGULAR:
+          subTotal += 2;
+          if (daysRented > 2) {
+            subTotal += (daysRented - 2) * 1.5;
+          }
+          break;
+
+        case NEW_RELEASE:
+          subTotal += daysRented * 3;
+          break;
+
+        case CHILDRENS:
+          subTotal += 1.5;
+          if (daysRented > 3) {
+            subTotal += (daysRented - 3) * 1.5;
+          }
+          break;
+
+        default:
+      }
+      return subTotal;
+    }
   };
 };
 
