b3b342a71eaed0cb3a37303c9a0ee48d429b0361
Matt Oberlin <moberlin8@gmail.com>
2017-03-06 10:32:29 +0800

    Prime numbers green bar, two tests.
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -3,6 +3,28 @@ describe('the prime numbers canary spec', () => {
     true.should.be.true();
   });
 
-  describe('a prime numbers function should', () => {
+  const primeFactorsOf = (number) => {
+    let factors = [];
+    if (number > 1) factors.push(2);
+    return factors;
+  }
+
+  describe.only('a prime factors of ', () => {
+    it('1 is none', () => {
+      primeFactorsOf(1).should.deepEqual([]);
+    });
+
+    it('2 are only 2', () => {
+      primeFactorsOf(2).should.deepEqual([2]);
+    });
+
+    it('3 are only 3');
+    it('4 are 2 and 2');
+    it('5 is only 5');
+    it('6 is 2 and 3');
+    it('7 is only 7');
+    it('8 are 2, 2, and 2');
+    it('9 are 3 and 3');
+
   });
 });
