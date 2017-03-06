1e62c0643cc49e17ddf80b21a92718dc91448497
JoongYeop Kim <jyeop@kr.ibm.com>
2017-03-06 15:50:19 +0800

    porting over code

--- server/async-02-line-count-with-promise/line-count.spec.js
+++ server/async-02-line-count-with-promise/line-count.spec.js
@@ -1,9 +1,28 @@
-describe('line count can be checked', () => {
-  const filespec = 'server/async-01-line-count-with-callback/line-count.js';
+import {lineCount} from './line-count';
 
-  it('with the done parameter like with a callback');
+describe.only('line count can be checked', () => {
+  const filespec = 'server/async-02-line-count-with-promise/line-count.js';
 
-  it('by returning the promise after .then');
+  it('with the done parameter like with a callback', done => {
+    const receiveCount = (numberOfLines) => {
+      numberOfLines.should.equal(14);
+      done();
+    };
 
-  it('by using eventually or fulfilledWith');
+    lineCount(filespec).then(receiveCount);
+  });
+
+  it('by returning the promise after .then', () => {
+
+    const receiveCount = (numberOfLines) => {
+      numberOfLines.should.equal(14);
+    };
+
+    lineCount(filespec).then(receiveCount);
+  });
+
+
+  it('by using eventually or fulfilledWith', () => {
+    return lineCount(filespec).should.eventually.equal(14);
+  });
 });
