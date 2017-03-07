9521e2f399fb902b4b7ae01fb23bc847d5b1f7b0
Matt Oberlin <moberlin8@gmail.com>
2017-03-07 09:51:11 +0800

    green fetch call api with current and prior
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/async-03-unusual-spending/fetch.spec.js
+++ server/async-03-unusual-spending/fetch.spec.js
@@ -1,5 +1,5 @@
 /*eslint dot-notation: "off"*/
-import {replace, when, verify} from '../../test-helper';
+import {replace, when} from '../../test-helper';
 
 describe.only('fetch ', () => {
   it('interacts with months and api', () => {
@@ -8,14 +8,16 @@ describe.only('fetch ', () => {
 
     const fetch = require('./fetch')['fetch'];
 
-    const current = {};
-    const prior = [];
+    const current = {month: 12, year: 2016};
+    const prior = {month: 11, year: 2016};
+    const priorMonthPayment = [{amount: 270, category: 'golf' }, {amount: 190, category: 'chess' }];
+    const currentMonthPayment = [{amount: 780, category: 'basketball' }, {amount: 290, category: 'bicycling' }];
 
     when(months.current()).thenReturn(current);
     when(months.prior()).thenReturn(prior);
 
     fetch('user-id');
-    verify(api('user-id', prior));
-    verify(api('user-id', current));
+    when(api('user-id', prior)).thenReturn(priorMonthPayment);
+    when(api('user-id', current)).thenReturn(currentMonthPayment);
   });
 });
