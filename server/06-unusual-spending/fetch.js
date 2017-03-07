2ed7a03bbcb91af1b12067414b444da837b2bc29
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-07 09:59:48 +0800

    added sync tests to line-count.spec.js

--- server/06-unusual-spending/fetch.js
+++ server/06-unusual-spending/fetch.js
@@ -2,15 +2,7 @@ import {current, prior} from './months';
 import {api} from './api';
 
 const fetch = (userId) => {
-  const currentMonth = current();
-  const priorMonth = prior();
-  const currentPayments = api(userId, currentMonth);
-  const priorPayments = api(userId, priorMonth);
-
-  //       fetch('user-id').should.deepEqual([currentPayments, priorPayments]);
-
-  return [currentPayments, priorPayments];
-
+  return [api(userId, current()), api(userId, prior())];
 };
 
 export {fetch};
