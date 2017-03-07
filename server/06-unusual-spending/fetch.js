50599832fe6aef1fbcb1ad829d004d8fbd82c92e
Victor Leung <victor.leung@digiflex-solutions.com>
2017-03-07 09:55:40 +0800

    added sync tests to line-count.spec.js

--- server/06-unusual-spending/fetch.js
+++ server/06-unusual-spending/fetch.js
@@ -2,10 +2,15 @@ import {current, prior} from './months';
 import {api} from './api';
 
 const fetch = (userId) => {
-    const currentMonth = current();
-    const priorMonth = prior();
-    api(userId, currentMonth);
-    api(userId, priorMonth);
+  const currentMonth = current();
+  const priorMonth = prior();
+  const currentPayments = api(userId, currentMonth);
+  const priorPayments = api(userId, priorMonth);
+
+  //       fetch('user-id').should.deepEqual([currentPayments, priorPayments]);
+
+  return [currentPayments, priorPayments];
+
 };
 
 export {fetch};
--- server/06-unusual-spending/fetch.spec.js
+++ server/06-unusual-spending/fetch.spec.js
@@ -18,4 +18,22 @@ describe.only('fetch', () => {
         verify(api('user-id', currentMonth));
         verify(api('user-id', priorMonth));
     });
+
+    it('returns correct result for stubbed input', () => {
+      const months = replace('./months');
+      const api = replace('./api')['api'];
+
+      const currentMonth = 'current-month';
+      const priorMonth = 'prior-month';
+      const currentPayments = 'current-payments';
+      const priorPayments = 'prior-payments';
+
+      when(months.current()).thenReturn(currentMonth);
+      when(months.prior()).thenReturn(priorMonth);
+      when(api('user-id', currentMonth)).thenReturn(currentPayments);
+      when(api('user-id', priorMonth)).thenReturn(priorPayments);
+
+      let fetch = require('./fetch').fetch;
+      fetch('user-id').should.deepEqual([currentPayments, priorPayments]);
+    });
 });
